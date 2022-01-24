204feae4bc83ae4d9933c09eafa655ac732664d4
devPark <ronny_park@naver.com>
2022-01-24 22:57:48 +0900

    로그인 API 설계

--- app.js
+++ app.js
@@ -1,49 +1,57 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const path = require('path');
 const userRouter = require('./routes/user');
-
 
 // express 인스턴스 생성
 const app = express();
-const PORT = 3000;
-// json request body 파싱
-app.use(express.json());
 
-app.use('/user',userRouter);
+app.set('port', process.env.PORT || 3000);
+
+app.use(
+  // 요청, 응답에 대한 좀 더 디테일한 로그 출력
+  morgan('dev'),
+  // 요청 경로가 "/" 일 때 public 폴더에 있는 정적 파일(html, css 등) 제공
+  express.static('/', path.join(__dirname, 'public')),
+  // json request body 파싱
+  express.json(),
+  // 요청 경로의 querystring 해석
+  express.urlencoded({ extended: false }),
+);
+
+app.use('/user', userRouter);
 
 // GET method route
 // respond with "Hello World!" when a GET request is made to the hompage
 app.get('/', function (req, res) {
-    res.send('Hello World!');
-    res.end();
-    console.log('it is not end');
+  res.send('Hello World!');
+  console.log('it is not end');
 });
 
 app.get('/user/:name', function (req, res) {
-    console.log(req.params);
+  console.log(req.params);
 });
 
-app.get('/search', function (req,res) {
-    console.log(req.query);
-    res.send(req.query);
-})
+app.get('/search', function (req, res) {
+  console.log(req.query);
+  res.send(req.query);
+});
 // POST method route
 // 애플리케이션의 홈페이지인 루트 라우트(/) 에서 POST 요청에 응답:
 app.post('/', function (req, res) {
-
-    res.send('Got a POST request');
+  res.send('Got a POST request');
 });
 // PUT method route
 // /user 라우트에 대한 PUT 요청에 응답:
 app.put('/user', function (req, res) {
-    res.send('Got a PUT request at /user');
+  res.send('Got a PUT request at /user');
 });
 // DELETE method route
 // /user 라우트에 대한 DELETE 요청에 응답:
 app.delete('/user', function (req, res) {
-    res.send('Got a DELETE request at /user');
+  res.send('Got a DELETE request at /user');
 });
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+app.listen(app.get('port'), () => {
+  console.log(`Example app listening at http://localhost:${app.get('port')}`);
 });
--- routes/user.js
+++ routes/user.js
@@ -1,83 +1,153 @@
 const express = require('express');
+
 const userRouter = express.Router();
 const { body, validationResult, check } = require('express-validator');
 
 const fakeUserDatabase = require('../fakeUserDatabase');
 
 userRouter.get('/', function (req, res) {
-    res.send('/user 잘 작동중!!!');
-    return;
+  res.send('/user 잘 작동중!!!');
 });
 
-function validatorErrorCallback (req, res, next) {
-    const errors = validationResult(req);
-    
-    if (!errors.isEmpty()) {
-        for (let error of errors.errors) {
-            if (error.msg === "이미 등록되어 있는 사용자명입니다." || error.msg === "이미 등록되어 있는 아이디입니다.") {
-                return res.status(409).send({"isExisted": true}); 
-            }
-        }
-
-        return res.status(400).json({ errors: errors.array()});
+// 에러 핸들링은 여기서
+function registerValidatorErrorCallback(req, res, next) {
+  const result = validationResult(req);
+  const hasErrors = !result.isEmpty();
+  if (hasErrors) {
+    for (const error of result.errors) {
+      if (
+        error.msg === '이미 등록되어 있는 사용자명입니다.' ||
+        error.msg === '이미 등록되어 있는 아이디입니다.'
+      ) {
+        return res.status(409).send({ isExisted: true });
+      }
     }
-    next();
+    throw new Error(result.array()[0].msg);
+  } else {
+    return next();
+  }
 }
 
-userRouter.post('/register', [
+userRouter.post(
+  '/register',
+  [
     check('username')
-        .exists()
-        .withMessage('반드시 입력해주시기 바랍니다.')
-        .isLength({ min: 3, max: 16 })
-        .withMessage('최소 3자 이상 최대 16자 이하로 입력해주세요.')
-        .custom((value, { req }) => {
-            for (let userInfo in fakeUserDatabase) {
-                if (fakeUserDatabase[userInfo].username === value) {
-                    throw new Error("이미 등록되어 있는 사용자명입니다.");
-                }
-            }
-        }),
-    check('id')
-        .exists()
-        .withMessage('반드시 입력해주시기 바랍니다.')
-        .bail()
-        .isEmail()
-        .withMessage('example@example.com 의 이메일 형식으로 입력해주세요.')
-        .custom((value, { req }) => {
-            for (let userInfo in fakeUserDatabase) {
-                if (fakeUserDatabase[userInfo].id === value) {
-                    throw new Error("이미 등록되어 있는 아이디입니다.");
-                }
-            }
-        }),
-    check('password')
-        .exists()
-        .withMessage('반드시 입력해주시기 바랍니다.')
-        .matches(/^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?])*.{8,16}$/)
-        .withMessage('숫자, 문자, 특수문자를 반드시 포함하여 최소 8자 이상 최대 16자 이하로 입력해주세요.'),
-    body('passwordConfirmation')
-        .custom((value, { req }) => {
-            if (!value) {
-                throw new Error('반드시 입력해주시기 바랍니다.');
-            }
-            if (value !== req.body.password) {
-                throw new Error('앞서 입력하신 비밀번호와 일치하지 않습니다. 다시 입력해주세요.');
-            }
+      .exists({ checkFalsy: true })
+      .withMessage('반드시 입력해주시기 바랍니다.')
+      .isLength({ min: 3, max: 16 })
+      .withMessage('최소 3자 이상 최대 16자 이하로 입력해주세요.')
+      .custom((value, { req }) => {
+        for (const userInfo in fakeUserDatabase) {
+          if (fakeUserDatabase[userInfo].username === value) {
+            throw new Error('이미 등록되어 있는 사용자명입니다.');
+          } else {
+            continue;
+          }
+        }
         return true;
-    }).bail(),
+      })
+      .bail(),
+    check('id')
+      .exists({ checkFalsy: true })
+      .withMessage('반드시 입력해주시기 바랍니다.')
+      .isEmail()
+      .withMessage('example@example.com 의 이메일 형식으로 입력해주세요.')
+      .custom((value, { req }) => {
+        for (const userInfo in fakeUserDatabase) {
+          if (fakeUserDatabase[userInfo].id === value) {
+            throw new Error('이미 등록되어 있는 아이디입니다.');
+          } else {
+            continue;
+          }
+        }
+        return true;
+      })
+      .bail(),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('반드시 입력해주시기 바랍니다.')
+      .matches(
+        /^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?])*.{8,16}$/,
+      )
+      .bail()
+      .withMessage(
+        '숫자, 문자, 특수문자를 반드시 포함하여 최소 8자 이상 최대 16자 이하로 입력해주세요.',
+      ),
+    body('passwordConfirmation')
+      .custom((value, { req }) => {
+        if (!value) {
+          throw new Error('반드시 입력해주시기 바랍니다.');
+        }
+        if (value !== req.body.password) {
+          throw new Error(
+            '앞서 입력하신 비밀번호와 일치하지 않습니다. 다시 입력해주세요.',
+          );
+        }
+        return true;
+      })
+      .bail(),
     check('tel')
-        .exists()
-        .withMessage('반드시 입력해주시기 바랍니다.')
-        .matches(/^\d{3}[-]{1}\d{4}[-]{1}\d{4}$/)
-        .withMessage('반드시 000-0000-0000 의 형식으로 입력해주세요.'),
-    validatorErrorCallback
-], function (req, res) {
+      .exists({ checkFalsy: true })
+      .withMessage('반드시 입력해주시기 바랍니다.')
+      .matches(/^\d{3}[-]{1}\d{4}[-]{1}\d{4}$/)
+      .withMessage('반드시 000-0000-0000 의 형식으로 입력해주세요.'),
+    registerValidatorErrorCallback,
+  ],
+  function (req, res) {
     // POST 방식으로 넘어오는 파라미터를 담고 있음.
-    const userInfo = req.body;
-
-    res.status(201).json({success: true});
+    const newUserInfo = req.body;
+    fakeUserDatabase.push(newUserInfo);
+    res.status(201).json({ success: true });
     console.log('회원가입 성공');
-    return;
+  },
+);
+
+// 에러 핸들링은 여기서
+function loginValidatorErrorCallback(req, res, next) {
+  const result = validationResult(req);
+  const hasErrors = !result.isEmpty();
+  if (hasErrors) {
+    throw new Error(result.array()[0].msg);
+  }
+  return next();
+}
+
+userRouter.post(
+  '/login',
+  [
+    check('id', '아이디를 입력해 주세요.').exists({ checkFalsy: true }),
+    check('password', '비밀번호를 입력해 주세요.').exists({ checkFalsy: true }),
+    loginValidatorErrorCallback,
+  ],
+  function (req, res) {
+    // req.body = {id: "", password: ""}
+    const loginUser = req.body;
+    fakeUserDatabase.forEach(userInfo => {
+      const isIdSame = userInfo.id === loginUser.id;
+      const isPasswordSame = userInfo.password == loginUser.password;
+
+      if (!isIdSame || !isPasswordSame) {
+        res.status(401).json({
+          success: false,
+          message:
+            '등록되지 않은 아이디이거나 아이디 또는 비밀번호를 잘못 입력하셨습니다..',
+        });
+        return false;
+      }
+      if (isIdSame && isPasswordSame) {
+        res.status(200).json({ success: true });
+        return false;
+      }
+    });
+    console.log('로그인 성공');
+  },
+);
+
+// validation check 에 대한 Error 핸들링
+userRouter.use(function (err, req, res, next) {
+  console.error(err);
+  console.log('error handling here');
+  res.status(400).json({ error: true });
 });
 
 module.exports = userRouter;
